5df9b8bc1cb8e66b1688cc2a00e5ecea8998aa1c
Yoan <yoamalaver@gmail.com>
2023-05-04 23:38:54 -0500

    Add logica de barra de busqueda

--- src/pages/components/navbar/navbar.jsx
+++ src/pages/components/navbar/navbar.jsx
@@ -1,29 +1,30 @@
-import { Disclosure } from '@headlessui/react'
+import { Disclosure } from '@headlessui/react';
 
 export default function Navbar() {
   return (
-    <Disclosure as="nav" className="bg-gray-900 mb-20">
+    <Disclosure as='nav' className='bg-gray-900 mb-20'>
       {({ open }) => (
         <>
-          <div className="mx-auto px-2 sm:px-6 lg:px-8">
-            <div className="relative flex h-16 items-center justify-between">
-              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-              </div>
-              <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-                <div className="flex flex-shrink-0 items-center">
+          <div className='mx-auto px-2 sm:px-6 lg:px-8'>
+            <div className='relative flex h-16 items-center justify-between'>
+              <div className='absolute inset-y-0 left-0 flex items-center sm:hidden'></div>
+              <div className='flex flex-1 items-center justify-center sm:items-stretch sm:justify-start'>
+                <div className='flex flex-shrink-0 items-center'>
                   <img
-                    className="block h-10 w-auto lg:hidden mr-4"
-                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-                    alt="Your Company"
+                    className='block h-10 w-auto lg:hidden mr-4'
+                    src='https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500'
+                    alt='Your Company'
                   />
                   <img
-                    className="hidden h-10 w-auto lg:block mr-4"
-                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-                    alt="Your Company"
+                    className='hidden h-10 w-auto lg:block mr-4'
+                    src='https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500'
+                    alt='Your Company'
                   />
                 </div>
                 <div>
-                  <h2 className='inline-block align-middle text-2xl font-sans font-medium'>Catálogo de películas</h2>
+                  <h2 className='inline-block align-middle text-2xl font-sans font-medium text-white'>
+                    Catálogo de películas
+                  </h2>
                 </div>
               </div>
             </div>
@@ -31,5 +32,5 @@ export default function Navbar() {
         </>
       )}
     </Disclosure>
-  )
+  );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,26 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import Navbar from './components/navbar/navbar'
-import Add from './components/addMovie/Add'
-import Movie from './components/filters/Movie'
-import Main from './components/main/main'
-import Category from './components/filters/Category'
-import Gender from './components/filters/Gender'
-import Pagination from './components/pagination/Pagination'
+import Image from 'next/image';
+import { Inter } from 'next/font/google';
+import Navbar from './components/navbar/navbar';
+import Add from './components/addMovie/Add';
+import Movie from './components/filters/Movie';
+import Main from './components/main/main';
+import Category from './components/filters/Category';
+import Gender from './components/filters/Gender';
+import Pagination from './components/pagination/Pagination';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
     <>
       <Navbar />
-      <main class="px-2 sm:px-6 lg:px-8">
+      <main class='px-2 sm:px-6 lg:px-8'>
         <Add />
-        <div class="flex flex-nowrap mb-10">
-          <Movie />
-          <Category  />
-          <Gender />
-        </div>
-        
-        <Main/>
+        <div class='flex flex-nowrap mb-10'></div>
+        <Movie />
+
         <Pagination />
       </main>
     </>
-  )
+  );
 }
